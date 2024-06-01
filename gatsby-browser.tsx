6b3ac972020a23e39a50a17dc8e9d07052f120eb
noahzm <2554594+noahzm@users.noreply.github.com>
2024-06-01 10:35:40 -0400

    Fix color scheme flashing

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -7,11 +7,15 @@ import { theme } from "./src/theme";
 
 export const wrapPageElement = ({ element }) => {
   return (
-    <>
-      <ColorSchemeScript forceColorScheme="light" />
-      <MantineProvider theme={theme} forceColorScheme="light">
-        {element}
-      </MantineProvider>
-    </>
+    <html data-mantine-color-scheme="light">
+      <head>
+        <ColorSchemeScript forceColorScheme="light" />
+      </head>
+      <body>
+        <MantineProvider theme={theme} forceColorScheme="light">
+          {element}
+        </MantineProvider>
+      </body>
+    </html>
   );
 };
