94f751b6b3cf73a6f98f5a0972de0c1fa16866c0
noahzm <2554594+noahzm@users.noreply.github.com>
2024-06-01 09:07:57 -0400

    Set default color scheme to light

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -6,5 +6,9 @@ import { MantineProvider } from "@mantine/core";
 import { theme } from "./src/theme";
 
 export const wrapPageElement = ({ element }) => {
-  return <MantineProvider theme={theme}>{element}</MantineProvider>;
+  return (
+    <MantineProvider theme={theme} defaultColorScheme="light">
+      {element}
+    </MantineProvider>
+  );
 };
